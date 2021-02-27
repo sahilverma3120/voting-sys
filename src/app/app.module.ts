8d52fe5c1286165cdc38002ac6866574513dd5b9
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-02-27 23:44:42 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+import { BlockViewComponent } from './components/block-view/block-view.component';
+import { VotesTableComponent } from './components/votes-table/votes-table.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { CreateVoteComponent } from './pages/create-vote/create-vote.component';
+import { PendingTransactionsComponent } from './pages/pending-transactions/pending-transactions.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlockchainViewerComponent,
+    BlockViewComponent,
+    VotesTableComponent,
+    SettingsComponent,
+    CreateVoteComponent,
+    PendingTransactionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
